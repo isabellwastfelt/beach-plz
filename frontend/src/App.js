6ce91e43215069843e4577c66a88265a50a68d55
Isabell Wästfelt <isabell@iMac-som-tillhor-Isabell.local>
2022-06-01 10:10:19 +0200

    added routes in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import { Provider } from 'react-redux'
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    // <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/registration' element={<Registration />} />
+        <Route path='/beaches' element={<Beaches />} />
+        <Route path='/review' element={<Review />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+    // </Provider>
   )
 }
