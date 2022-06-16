977ad1d8ca66dc26e3ac13369b3104b6c8065c7d
JensBergqvist <95050484+JensBergqvist@users.noreply.github.com>
2022-06-16 14:50:19 +0200

    Added logout (#2)

--- frontend/src/pages/Logout.js
+++ frontend/src/pages/Logout.js
@@ -1,1 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Navigate } from 'react-router-dom'
+import { setCookie } from 'utils/cookieHelper'
+
+const Logout = () => {
+  useEffect(() => {
+    setCookie('accessToken', null, 0)
+  }, [])
+
+  return <Navigate to="/" />
+}
+
+export default Logout
