277122adf3aed2e772fb27197fdf5909d9e88d47
Hanna Wallberg <91497486+HWallberg@users.noreply.github.com>
2022-06-17 11:04:44 +0200

    Feature/filter (#5)
    * working filter function. Styling nav bar. New css file for navbar.
    
    * new css file for main. burger menu done

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,15 +1,11 @@
 import React from 'react'
 
-import { Header } from '../components/Header'
 import { ProfileBeaches } from '../components/ProfileBeaches'
 import { ProfileFeed } from '../components/ProfileFeed'
 
 export const Profile = () => {
   return (
     <div>
-      <div>
-        <Header />
-      </div>
       <>
         <h1>Din profil</h1>
         <div>
